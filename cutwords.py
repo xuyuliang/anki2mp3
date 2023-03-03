f7379fda2647d35149a576125a5132f727ca5e2f
xuyuliang <xuyuliang@gmail.com>
2023-03-03 16:56:57 +0800

    0303

--- cutwords.py
+++ cutwords.py
@@ -1,5 +1,17 @@
+import configparser
 import os
 import csv
+
+
+config = configparser.RawConfigParser()
+config.read("config.ini",encoding='utf-8')
+VOWELS = eval(config['characters']['vowels'])
+CONSONANTS = eval(config['characters']['consonants'])
+HALF_VOWEL = eval(config['characters']['half_vowel'])
+DOUBLE_CONSONANTS =eval(config['characters']['double_consonants'])
+
+
+
 def isEnglishChar(s):
     try:
         s.encode(encoding='utf-8').decode('ascii')
@@ -48,21 +60,80 @@ def extract_english_letters(realword,astring):
 def cutbypronuncation(myword):
     if len(myword) <= 5:
         return myword
+    list_myword = list(myword)
+    list_newword = list_myword.copy()
+    vowels_position =[] 
+    for i in range(len(list_myword)):
+        if list_myword[i] in VOWELS:
+            vowels_position.append(i)
+        if (list_myword[i] in HALF_VOWEL) and (i>0) and (i< (len(list_myword)-1)):
+            vowels_position.append(i)
+
+    print(vowels_position)
+    dict_consonants_group= {}
+    for i in range(len(vowels_position)):
+        if i+1 < len(vowels_position):
+            consonants_group = list_myword[vowels_position[i]+1:vowels_position[i+1]]
+            dict_consonants_group.update({vowels_position[i]+1:consonants_group})
+        # else:
+        #     consonants_group = list_myword[vowels_position[i]+1:]
+
+        print('cons',consonants_group)
+    print(dict_consonants_group)
+    list_newword = list_myword.copy()
+    offset = 0
+    # for item in dict_consonants_group:
+    #     if len(item[1]) == 1:
+    #         list_newword.insert(offset+item[0]+1,'.')
+    #         offset += 1
+    #     if len(item[1]) == 2:
+    #         if ''.join(item[1]) in DOUBLE_CONSONANTS:
+    #             list_newword.insert(offset+item[0]+1,'.')
+    #         else:
+    #             list_newword.insert(offset+item[0]+2,'.')
+    #         offset += 1
+    #     if len(item[1]) == 3:
+    #         if item[1][:2] in DOUBLE_CONSONANTS:
+    #             list_newword.insert(offset+item[0]+3,'.')
+    #         if item[1][1:] in DOUBLE_CONSONANTS:
+    #             list_newword.insert(offset+item[0]+1,'.')
+    #         else:
+    #             print('3个辅音，没有双辅音，是不是需要检查一下了')
+    #             list_newword.insert(offset+item[0] +2,'.')
+    #         offset += 1
+    # print(''.join(list_newword))
+
+
+
+
+
+
+
+
+            
+
     
     
 def main():
-    '''
-    word,text = 'tantrum',' tan晒黑+trum没有p，trump是川普、胜出 的意思。联想：小孩晒黑了发脾气'
+    # '''
+    # word,text = 'tantrum',' tan晒黑+trum没有p，trump是川普、胜出 的意思。联想：小孩晒黑了发脾气'
     # word,text = 'apocalypse','apo.ca.lyp.se'
     # word,text = 'tantrum','tan.trum'
-    res = extract_english_letters(word,text)
-    print('result:',res)
-    '''
-    file = open('./testwords4cut.txt','r',encoding='utf-8')
-    for line in file:
-        aword = str.strip(line)
-        print(aword)
-        print(cutbypronuncation(aword))
+    # word,text = 'tantrum','tantrum 是什么[`tantrum]'
+    # res = extract_english_letters(word,text)
+    # print('result:',res)
+    # '''
+
+    # i = 0
+    # file = open('./testwords4cut.txt','r',encoding='utf-8')
+    # for line in file:
+    #     aword = str.strip(line)
+    #     print(aword)
+    #     print(cutbypronuncation(aword))
+    #     if i > 20:
+    #         quit()
+    aword = 'coaincidented'
+    cutbypronuncation(aword)
 
 if __name__ == "__main__" :
     main()
